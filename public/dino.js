dc70559e04da36b5f47215864cafc20f96db6f2e
arj || Diode ;) <arjunanmr101@gmail.com>
2020-04-30 15:56:00 +0530

    tuning gravity

--- public/dino.js
+++ public/dino.js
@@ -31,7 +31,7 @@ class Dino {
   }
   jump() {
     if (this.vel.y == 0) {
-      const vel_y = window.innerHeight * 0.032;
+      const vel_y = window.innerWidth * 0.015;
       this.vel.y -= vel_y;
     }
   }
