a9ead34cacb9d5789ad2ab2a41168c41b4f2446e
arj || Diode ;) <arjunanmr101@gmail.com>
2020-04-30 14:16:33 +0530

    added basic socket.io code 😃!

--- server.js
+++ server.js
@@ -9,3 +9,14 @@ console.log(`Starting server at port ${port}...`);
 const server = app.listen(port, () => {
   console.log(`Listening at port ${port}!`);
 });
+
+app.use(express.static("public"));
+
+const io = socket(server);
+
+io.sockets.on("connection", (socket) => {
+  console.log(`We have a client ${socket.id} !`);
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected `);
+  });
+});
