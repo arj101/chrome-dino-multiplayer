50863435c12e414c9dc4dbb240503f9856b021e2
arj || Diode ;) <arjunanmr101@gmail.com>
2020-05-13 13:16:43 +0530

    added material library

--- public/index.js
+++ public/index.js
@@ -8,6 +8,8 @@ let userName;
 
 let game_timer;
 
+mdc.ripple.MDCRipple.attachTo(document.querySelector(".foo-button"));
+
 socket.emit("query", { type: "game" }, (reply) => {
   game = reply;
   if (game.status == "no_games") {
