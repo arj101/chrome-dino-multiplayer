47fd658e7814dba904276ea7e1d945ca2eb6dfef
Arj || Diode <arjunanmr101@gmail.com>
2020-05-25 15:44:15 +0530

    placed js files in public/game to public/game/js

---
+++ public/game/js/obst.js
@@ -0,0 +1,26 @@
+class Obstacle {
+  constructor(x, y, s, h = undefined) {
+    if (h != undefined) {
+      this.h = h;
+    } else {
+      this.h = s;
+    }
+    this.s = s;
+    this.pos = createVector(x, y - this.h);
+    this.w = (this.s * 3) / 4;
+  }
+
+  update() {
+    this.pos.x -= dino_speed;
+  }
+
+  show() {
+    push();
+    fill(255, 50, 50);
+    noStroke();
+    rect(this.pos.x, this.pos.y, this.w, this.h);
+    // fill(100);
+    // rect(this.pos.x, this.pos.y, 10, 10);
+    pop();
+  }
+}
