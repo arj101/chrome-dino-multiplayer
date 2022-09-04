9ab10b8c93c949dbcc1cdabba93c21f35d2f288c
arduino explorer <arjunanmr101@gmail.com>
2022-09-04 22:30:13 +0530

    use port number from env var

--- dino-backend/src/main.rs
+++ dino-backend/src/main.rs
@@ -94,7 +94,8 @@ async fn handle_connection(
 
 #[tokio::main]
 async fn main() -> Result<(), IoError> {
-    let addr = "127.0.0.1:8080";
+    let port = std::env::var("PORT").unwrap_or("8080".to_string()).parse::<usize>();
+    let addr = &format!("127.0.0.1:{}", port.unwrap_or(8080));
     let config = ConfigOptions {
         session: SessionConfig {
             max_users: 20,
